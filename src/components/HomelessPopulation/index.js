2ec2a2c55917859fe6846e5e3b510959570984d3
Clem Freeman <clemfree@gmail.com>
2017-04-23 12:46:05 -0700

    Convert Population component to class, remove years

--- src/components/HomelessPopulation/index.js
+++ src/components/HomelessPopulation/index.js
@@ -1,7 +1,9 @@
 /* eslint-disable react/jsx-boolean-value */
 import React from 'react';
+import { connect } from 'react-redux';
 import { BarChart, Bar, XAxis, YAxis, Text, Legend, ResponsiveContainer } from 'recharts';
 import styles from './styles.css';
+import { fetchPopulationData } from '../../state/Population/actions';
 
 // TODO - Wire-up w/ redux
 const propsData = [
@@ -9,8 +11,7 @@ const propsData = [
   { name: 'White', 'General Population': 70, Homeless: 30 },
   { name: 'Not Reported', 'General Population': 10, Homeless: 20 },
 ];
-const propsYears = [2011, 2013, 2015];
-const propsActive = 2011;
+
 const propsCateg = ['Ethnicity', 'Veteran Status', 'Disability', 'Age', 'Gender'];
 const propsSelected = 'Veteran Status';
 
@@ -28,78 +29,82 @@ const axisLabel = options => (
 );
 
 
-const HomelessPopulation = props => (
-  <div className="HomelessPopulation homelesspopulation-container dataViz-container-500">
-    <h2>Compare the population of Multnomah County in 2015 to 
+class HomelessPopulation extends React.Component {
+  componentDidMount() {
+    this.props.loadData();
+  }
+  render() {
+    return (
+      <div className="HomelessPopulation homelesspopulation-container dataViz-container-500">
+        <h2>Compare the population of Multnomah County in 2015 to
     the homeless</h2>
-    <div className="selector">
-      <select name="category" value={propsSelected}>
-        {
+        <div className="selector">
+          <select name="category" value={propsSelected}>
+            {
           propsCateg.map(item => (
             <option key={item} value={item} >{item}</option>
           ))
         }
-      </select>
-    </div>
-    <ResponsiveContainer width="100%" height={'100%'} minHeight={450} >
-      <BarChart
-        data={propsData}
-        layout={'vertical'}
-        margin={{ top: 65, right: 10, left: 10, bottom: 0 }}
-      >
-        <Legend 
-          verticalAlign={'bottom'}
-          align={'center'}
-          layout={'vertical'}
-          iconSize={18}
-          wrapperStyle={{ top: 0 }}
-        />
-        <XAxis 
-          type="number"
-          axisLine={false} 
-          tickLine={false}
-          tick={false}
-        />
-        <YAxis 
-          type="category" 
-          tickLine={false}
-          dataKey="name"
-          tick={axisLabel}
-          mirror={true}
-          axisLine={false}
-        />
-        <Bar 
-          dataKey="General Population" 
-          fill={COLORS[1]} 
-          label={valueLabel}
-          legendType={'circle'}
-          barSize={24}
-        />
-        <Bar 
-          dataKey="Homeless" 
-          fill={COLORS[0]} 
-          label={valueLabel}  
-          legendType={'circle'}
-          barSize={29}
-        />
-      </BarChart>
-    </ResponsiveContainer>
-    <div className="Years-List">
-      <ul>
-        {
-          propsYears.map((item) => {
-            const active = item === propsActive ? 'active' : '';
-            return (
-              <li key={item}>
-                <a className={`${active}`}>{item}</a>
-              </li>
-            );
-          })
-        }
-      </ul>
-    </div>
-  </div>
-);
+          </select>
+        </div>
+        <ResponsiveContainer width="100%" height={'100%'} minHeight={450} >
+          <BarChart
+            data={propsData}
+            layout={'vertical'}
+            margin={{ top: 65, right: 10, left: 10, bottom: 0 }}
+          >
+            <Legend
+              verticalAlign={'bottom'}
+              align={'center'}
+              layout={'vertical'}
+              iconSize={18}
+              wrapperStyle={{ top: 0 }}
+            />
+            <XAxis
+              type="number"
+              axisLine={false}
+              tickLine={false}
+              tick={false}
+            />
+            <YAxis
+              type="category"
+              tickLine={false}
+              dataKey="name"
+              tick={axisLabel}
+              mirror={true}
+              axisLine={false}
+            />
+            <Bar
+              dataKey="General Population"
+              fill={COLORS[1]}
+              label={valueLabel}
+              legendType={'circle'}
+              barSize={24}
+            />
+            <Bar
+              dataKey="Homeless"
+              fill={COLORS[0]}
+              label={valueLabel}
+              legendType={'circle'}
+              barSize={29}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+    );
+  }
+}
 
-export default HomelessPopulation;
-    
+// HomelessPopulation.propTypes = {
+//   ethnicityData
+// }
+
+const mapDispatchToProps = dispatch => ({
+  loadData: () => fetchPopulationData(dispatch),
+});
+
+const mapStateToProps = state => ({
+  ethnicityData: state.population.ethnicityData,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomelessPopulation);
