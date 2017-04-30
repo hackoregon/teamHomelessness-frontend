cf9b43f0fe8e343019d2fce418d0a4cc47fc33d5
Clem Freeman <clemfree@gmail.com>
2017-04-30 16:00:24 -0700

    Complete layout and props for TitleNav

--- src/components/Reuseable/TitleNav/TitleNav.js
+++ src/components/Reuseable/TitleNav/TitleNav.js
@@ -1,20 +1,46 @@
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 
-const container = {
-  display: 'absolute',
+const titleContainer = {
+  backgroundColor: '#F3F2F3',
+  height: '124px',
+  margin: 0,
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
 };
 
-class TitleNav extends React.Component {
-  render() {
-    return (
-      <div>
-        <div style={container}>
-          <h1>{this.props.title}</h1>
-        </div>
-        {this.props.children}
-      </div>
-    );
-  }
-}
+const title = {
+  textAlign: 'center',
+  fontWeight: 'bold',
+  margin: '0 100px',
+};
+
+const link = {
+  margin: '0 10px',
+};
+
+const TitleNav = props => (
+  <div>
+    <div style={titleContainer}>
+      <Link to={props.prev} style={link}>
+        <img src="../../assets/arrow-left.svg" />
+      </Link>
+      <h1 style={title}>{props.title}</h1>
+      <Link to={props.next} style={link}>
+        <img src="../../assets/arrow-right.svg" />
+      </Link>
+    </div>
+    {props.children}
+  </div>
+);
+
+TitleNav.propTypes = {
+  title: PropTypes.string.isRequired,
+  prev: PropTypes.string.isRequired,
+  next: PropTypes.string.isRequired,
+  children: React.PropTypes.element.isRequired,
+};
 
 export default TitleNav;
