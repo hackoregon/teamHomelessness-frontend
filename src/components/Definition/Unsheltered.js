f3642700dab4371fbdac28ee6237e2e472735511
Greg <adamsgreg100@gmail.com>
2017-04-30 16:33:27 -0700

    update Definition for dynamic copy count & year

--- src/components/Definition/Unsheltered.js
+++ src/components/Definition/Unsheltered.js
@@ -1,13 +1,33 @@
 import React from 'react';
 
-const Unsheltered = () => (
-  <div>
-    <h2>Unsheltered</h2>
-    <p>
-      People are unsheltered when their primary nighttime residence is a public or private space not designed for sleeping—such as a parked car, an abandoned building, or the doorway of a commercial storefront.
-    </p>
-  </div>
-);
+const Unsheltered = (props) => {
+  
+  const { year } = props;
+  const { rawCount, rawTotal } = props.data;
+ 
+  return (
+    <div>
+      <h2>Unsheltered</h2>
+      <p>
+        {year === 2015 &&
+          `People are unsheltered when their primary nighttime residence is a public or private space not designed for sleeping—such as a parked car, an abandoned building, or the doorway of a commercial storefront. On the night of January 28, ${year}, ${rawCount.toLocaleString()} of the ${rawTotal.toLocaleString()} who met the HUD definition of homelessness were unsheltered.`
+        }
+
+        {year === 2013 &&
+          `People are unsheltered when their primary nighttime residence is a public or private space not designed for sleeping—such as a parked car, an abandoned building, or the doorway of a commercial storefront. On the night of January 30, ${year}, ${rawCount.toLocaleString()} of the ${rawTotal.toLocaleString()} who met the HUD definition of homelessness were unsheltered.`
+        }
+
+        {year === 2011 &&
+          `People are unsheltered when their primary nighttime residence is a public or private space not designed for sleeping—such as a parked car, an abandoned building, or the doorway of a commercial storefront. On the night of January 26, ${year}, ${rawCount.toLocaleString()} of the ${rawTotal.toLocaleString()} who met the HUD definition of homelessness were unsheltered.`
+        }
+
+        {year === 2009 &&
+          'Individuals and families who are homeless and sleeping outside, in vehicles, in abandoned buildings, or other places not intended for human habitation. This includes streets, sidewalks, parks, alleys, transportation depots or other parts of transportation systems, all-night commercial establishments (e.g. movie theaters, laundromats, restaurants), abandoned buildings, farm outbuildings, caves, campgrounds, vehicles, and similar places.'
+        }
+      </p>
+    </div>
+  );
+};
 
 
 export default Unsheltered;
