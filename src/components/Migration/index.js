e4ffe971916ecb04a74da7be1a872b6518f63a2d
Clem Freeman <clemfree@gmail.com>
2017-05-01 10:37:16 -0700

    Refactor migration page

--- src/components/Migration/index.js
+++ src/components/Migration/index.js
@@ -2,29 +2,36 @@ import React from 'react';
 import styles from './styles.css';
 import ArrivalPieChart from './ArrivalPieChart';
 import OriginTreemap from './OriginTreemap';
+import { TitleNav } from '../Reuseable';
+import StoryCard from '@hackoregon/component-library/lib/StoryCard/StoryCard';
+import shared from '../shared.styles';
 
-const Migration = () => {
-  return (
-    <div className="Migration">
-      <div className="migration-container">
-        <h1>Homeless Migration</h1>
-        <h2>In 2015, among the newly arrived unsheltered</h2>
+const Migration = () => (
+  <TitleNav
+    title="Homeless Migration"
+    prev="/homeless-population"
+    next="/services211"
+  >
+    <StoryCard>
+      <div className="Migration">
+        <div className="migration-container">
+          <h2>In 2015, among the newly arrived unsheltered</h2>
+        </div>
+        <ArrivalPieChart />
+        <div className="migration-container" >
+          <h2>were homeless on arrival</h2>
+          <p style={shared.text}>This number represents the percentage of the unsheltered population that moved to Multnomah County in the two years since the previous PIT survey (i.e. from 2013 to 2015).</p>
+          <h2 style={shared.header}>And most of them came from outside the Pacific Northwest</h2>
+        </div>
+        <OriginTreemap />
+        <div className="migration-container" >
+          <p style={shared.footnote}>*Portland Metro Areas include Clackamas, Washington, and Clark Counties</p>
+          <p style={shared.footnote}>*Those who responded Portland migrated from East County.</p>
+          <p style={shared.footnote}>This data extrapolates demographics from the unsheltered population to all homeless.</p>
+        </div>
       </div>
-      <ArrivalPieChart />
-      <div className="migration-container" >
-        <h2>were homeless on arrival</h2>
-        <p>This number represents the percentage of the unsheltered population that moved to Multnomah County in the two years since the previous PIT survey (i.e. from 2013 to 2015).</p>
-        <h2>And most of them came from outside the Pacific Northwest</h2>
-      </div>
-      <OriginTreemap />
-      <div className="migration-container" >
-        <p>*Portland Metro Areas include Clackamas, Washington, and Clark Counties</p>
-        <p>*Those who responded Portland migrated from East County.</p>
-        <p>This data extrapolates demographics from the unsheltered population to all homeless.</p>
-      </div>
-      
-    </div>
-  );
-};
+    </StoryCard>
+  </TitleNav>
+);
 
 export default Migration;
