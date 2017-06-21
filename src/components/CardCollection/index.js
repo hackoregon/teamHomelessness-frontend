59b28ce6191f6184ad3ea4b9b19b1b6569a2ac23
Clem Freeman <clemfree@gmail.com>
2017-06-20 20:00:49 -0700

    Create CardCollection component

---
+++ src/components/CardCollection/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Definition from '../Definition';
+import HomelessPopulation from '../HomelessPopulation';
+import Migration from '../Migration';
+import UnaccompaniedYouth from '../UnaccompaniedYouth';
+import Women from '../Women';
+import Services211 from '../Services211';
+
+const CardCollection = () => (
+  <div>
+    <Definition />
+    <HomelessPopulation />
+    <Migration />
+    <UnaccompaniedYouth />
+    <Women />
+    <Services211 />
+  </div>
+);
+
+export default CardCollection;
