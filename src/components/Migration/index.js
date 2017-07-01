5827a0c071dd36926869bb5eb82a0d8a1abc397e
Clem Freeman <clemfree@gmail.com>
2017-06-30 21:00:43 -0700

    Switch migration to halfDonut

--- src/components/Migration/index.js
+++ src/components/Migration/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import styles from './styles.css';
-import ArrivalPieChart from './ArrivalPieChart';
+import HalfDonutChart from '../Reuseable/HalfDonutChart/HalfDonutChart';
 import OriginTreemap from './OriginTreemap';
 import { TitleNav } from '../Reuseable';
 import StoryCard from '@hackoregon/component-library/lib/StoryCard/StoryCard';
 import shared from '../shared.styles';
+
+const arrivalData = [{
+  name: '2015 - 2017',
+  data: [
+    {
+      name: 'Arrived Homeless',
+      value: 12,
+    },
+    {
+      name: 'Arrived Sheltered',
+      value: 88,
+    },
+  ],
+}];
 
 const Migration = () => (
   <TitleNav
@@ -13,9 +27,9 @@ const Migration = () => (
     <StoryCard>
       <div className="Migration">
         <div className="migration-container">
-          <h2>In 2015, among the newly arrived unsheltered</h2>
+          <h2>Between 2015 and 2017, among the newly arrived unsheltered</h2>
         </div>
-        <ArrivalPieChart />
+        <HalfDonutChart dataSets={arrivalData} renderLinks={false} legend={false} />
         <div className="migration-container" >
           <h2>were homeless on arrival</h2>
           <p style={shared.text}>This number represents the percentage of the unsheltered population that moved to Multnomah County in the two years since the previous PIT survey (i.e. from 2013 to 2015).</p>
